139e8f3bd1526638750425378a25582ce545ba78
luisalomia <luisfidel44@gmail.com>
2022-08-23 14:03:22 -0500

    rename cardDescription

--- src/pages/shoesDescription/components/Description.jsx
+++ src/pages/shoesDescription/components/Description.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import CardDescription from "../../../components/cardDescription/components/cardDescription";
+import CardDescription from "../../../components/descriptionCard/components/CardDescription";
 import { getIdProductsServices } from "../../../services/getProductServices";
 
 const Description = () => {
