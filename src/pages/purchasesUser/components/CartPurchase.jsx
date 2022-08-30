c2837c9f46d12f652a06b9045d7a7414a074285a
luisalomia <luisfidel44@gmail.com>
2022-08-29 21:16:09 -0500

    purchase

--- src/pages/purchasesUser/components/CartPurchase.jsx
+++ src/pages/purchasesUser/components/CartPurchase.jsx
@@ -1,28 +1,20 @@
-import { useLocalStorage } from "../../../hooks/useLocalStorege";
 import style from "../styles/cartPurchases.module.css";
 
 const CartPurchase = ({ item }) => {
-  const [token] = useLocalStorage("token", "");
-  const { username } = token;
-
   return (
     <>
-      {item.carts
-        .filter((item) => item.username === username)
-        .map((item) =>
-          item.cart.map((item, index) => (
-            <div key={index} className={style.content}>
-              <div className={style.text}>
-                <h3 className={style.text}>{item.productname}</h3>
-                <p className={style.text}>{item.category}</p>
-                <p className={style.text}>{item.price}</p>
-              </div>
-              <div className={style.contentImage}>
-                <img src={item.image1} alt={item.productname} />
-              </div>
-            </div>
-          ))
-        )}
+      {item.cart.map((item, index) => (
+        <div key={index} className={style.content}>
+          <div className={style.text}>
+            <h3 className={style.text}>{item.productname}</h3>
+            <p className={style.text}>{item.category}</p>
+            <p className={style.text}>{item.price}</p>
+          </div>
+          <div className={style.contentImage}>
+            <img src={item.image1} alt={item.productname} />
+          </div>
+        </div>
+      ))}
     </>
   );
 };
--- src/pages/purchasesUser/components/PurchasesUser.jsx
+++ src/pages/purchasesUser/components/PurchasesUser.jsx
@@ -3,20 +3,33 @@ import { getPurchase } from "../../../services/catrsServices";
 import CartPurchase from "./CartPurchase";
 import style from "../styles/purcheseUser.module.css";
 
+import { useLocalStorage } from "../../../hooks/useLocalStorege";
+
 const PurchasesUser = () => {
-  const [purchases, setPurchase] = useState();
+  const [purchases, setPurchase] = useState([]);
+  const [buy, setBuy] = useState();
+  const [token] = useLocalStorage("token", "");
+  const { username } = token;
+  const array = [];
 
   useEffect(() => {
     getPurchase().then((resp) => {
-      setPurchase(resp.data);
+      setBuy(
+        resp.data.map((item) =>
+          item.carts.map(
+            (item) => item.username === username && array.push(item)
+          )
+        )
+      );
+      setPurchase(array);
     });
   }, []);
 
   return (
     <div className={style.content}>
       <h2 className={style.title}>Registro de Compras</h2>
-      {purchases?.map((item) => (
-        <div key={item._id} className={style.contentCard}>
+      {purchases?.map((item, index) => (
+        <div key={index} className={style.contentCard}>
           <CartPurchase item={item} />
         </div>
       ))}
